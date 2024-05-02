b28ea254f1a514307f4b5ae6b553a8130faec346
Cornelia Andersen <s210955@student.dtu.dk>
2024-05-02 14:10:07 +0200

    convert

---
+++ convert.py
@@ -0,0 +1,11 @@
+file = open("dump.sql","r")
+import re
+li = ""
+
+dump2 = open("dump2.sql", "a")
+for l in file.readlines():
+    l2 = l.replace("\"", "").replace("TEXT","VARCHAR(50)").replace("`", "").replace("PRAGMA foreign_keys=OFF;", "SET FOREIGN_KEY_CHECKS=0;").replace("BEGIN TRANSACTION;","").replace("TIMESTAMP", "DATETIME")
+    l2 = re.sub(r'(?<=\d)[T]', " ",l2)
+    l2 = re.sub(r'\.\d\d\d[Z]', "",l2)
+    dump2.write(l2)
+
