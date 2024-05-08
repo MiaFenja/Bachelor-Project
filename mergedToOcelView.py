7e503416d64d328ad9be8879fae3789ac33f312a
Cornelia Andersen <s210955@student.dtu.dk>
2024-05-08 11:32:44 +0200

    mmmmm

--- mergedToOcelView.py
+++ mergedToOcelView.py
@@ -7,6 +7,8 @@ def drop_views(c):
     c.execute("""DROP VIEW IF EXISTS event_OCEL""")
     c.execute("""DROP VIEW IF EXISTS object_OCEL""")
     c.execute("""DROP VIEW IF EXISTS objectObject_OCEL""")
+    c.execute("""DROP VIEW IF EXISTS event_map_type_Ocel""")
+    c.execute("""DROP VIEW IF EXISTS object_map_type_OCEL""")
     connect.commit()
 
 
@@ -15,8 +17,12 @@ def create_view_event(c):
                  FROM event NATURAL JOIN eventType WHERE event.eventTypeID = eventType.eventTypeID""")
     connect.commit()
 
+def get(data):
+    return data.replace(" ", "_")
 def create_view_eventMapType(c):
-    return
+    connect.create_function("get",1,get)
+    c.execute("CREATE VIEW event_map_type_Ocel AS SELECT eventType AS ocel_type, get(eventType) AS event_type_map FROM eventType")
+    
 
 def create_view_object(c):
     c.execute("""CREATE VIEW object_OCEL AS SELECT object.objectID AS ocel_id, objectType.objectType AS ocel_type 
@@ -29,7 +35,7 @@ def create_view_object_object(c):
                  FROM objectObject""")
 
 def create_view_objectMapType(c):
-    return
+    c.execute("CREATE VIEW object_map_type_Ocel AS SELECT objectType AS ocel_type, get(objectType) AS object_type_map FROM objectType")
 
 def create_view_eventObject(c):
     return
@@ -51,6 +57,10 @@ create_view_eventOcelTypes(c)
 create_view_objectOcelTypes(c)
 
 c.execute("select * from objectObject_OCEL")
+print(c.fetchall())
+c.execute("select * from event_map_type_Ocel ")
+print(c.fetchall())
+c.execute("select * from object_map_type_Ocel")
 print(c.fetchall())
 
 
