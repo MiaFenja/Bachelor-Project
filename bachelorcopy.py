5d80c587a069e923d36c462bc68a6b3c42f709af
Cornelia Andersen <s210955@student.dtu.dk>
2024-05-08 09:50:52 +0200

    small changes

--- bachelorcopy.py
+++ bachelorcopy.py
@@ -2,7 +2,7 @@ import sqlite3
 
 connect = sqlite3.connect("new.sqlite")
 c = connect.cursor()
-c.execute("ATTACH DATABASE 'ContainerLogistics.sqlite' as 'ocelbase'")
+c.execute("ATTACH DATABASE 'OCEL_Simple_Database.db' as 'ocelbase'")
 
 
 def create_eventType_Ocel(c):
@@ -257,5 +257,16 @@ def create_eventAttributeValue(c):
 
 create_eventType_Ocel(c)
 create_event_Ocel(c)
-
+create_objectObject_Ocel(c)
+create_eventObject_Ocel(c)
+create_objectType(c)
+create_object(c)
+create_objectRelationEvent(c)
+create_objectAttribute(c)
+create_objectAttributeValue(c)
+create_objectAttributeValueEvent(c)
+create_eventAttribute(c)
+create_eventAttributeValue(c)
+c.execute("select * from objectObject")
+fetchh = c.fetchall()
 
