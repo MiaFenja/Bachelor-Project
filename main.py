88aae80cb5c1ca1ee3cc04828d2160b905b2b15f
Cornelia Andersen <s210955@student.dtu.dk>
2024-05-23 14:07:15 +0200

    name changes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sqlite3
 import ocelToMerged as o1m
+import ocedToMerged as o2m
 def create_from_ocel(c,base,connect,type):
     str = ""
     if type == 'ocel':
@@ -21,18 +22,18 @@ def create_from_ocel(c,base,connect,type):
         o1m.create_eventAttribute_Ocel(c,connect)
         o1m.create_eventAttributeValue_Ocel(c,connect)
     else:
-        o1m.create_eventType_Oced(c,connect)
-        o1m.create_event_Oced(c,connect)
-        o1m.create_objectObject_Oced(c,connect)
-        o1m.create_eventObject_Oced(c,connect)
-        o1m.create_objectType_Oced(c,connect)
-        o1m.create_object_Oced(c,connect)
-        o1m.create_objectRelationEvent_Oced(c,connect)
-        o1m.create_objectAttribute_Oced(c,connect)
-        o1m.create_objectAttributeValue_Oced(c,connect)
-        o1m.create_objectAttributeValueEvent_Oced(c,connect)
-        o1m.create_eventAttribute_Oced(c,connect)
-        o1m.create_eventAttributeValue_Ocel(c,connect)
+        o2m.create_eventType_Oced(c,connect)
+        o2m.create_event_Oced(c,connect)
+        o2m.create_objectObject_Oced(c,connect)
+        o2m.create_eventObject_Oced(c,connect)
+        o2m.create_objectType_Oced(c,connect)
+        o2m.create_object_Oced(c,connect)
+        o2m.create_objectRelationEvent_Oced(c,connect)
+        o2m.create_objectAttribute_Oced(c,connect)
+        o2m.create_objectAttributeValue_Oced(c,connect)
+        o2m.create_objectAttributeValueEvent_Oced(c,connect)
+        o2m.create_eventAttribute_Oced(c,connect)
+        o2m.create_eventAttributeValue_Ocel(c,connect)
 def main():
     connect = sqlite3.connect("merged.sqlite")
     c = connect.cursor()
